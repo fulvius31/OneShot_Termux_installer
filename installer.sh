ff211455c4e7a8b2a041a4e7948a6d904b339206
Alessandro Sangiorgi <mail@alessandrosangiorgi.net>
2023-11-30 17:23:07 -0600

    Update installer.sh
    check if the commands have errors

--- installer.sh
+++ installer.sh
@@ -1,9 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg install -y root-repo 
-pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone --depth 1 https://github.com/fulvius31/OneShot OneShot
+execute_command() {
+    if ! "$@"; then
+        echo "Error: Failed to execute $*"
+        exit 1
+    fi
+}
 
-chmod +x OneShot/oneshot.py
+execute_command pkg update -y
+execute_command pkg install -y root-repo
+execute_command pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python OneShot/oneshot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd OneShot && git pull)\n###############################################\n"
+execute_command git clone --depth 1 https://github.com/fulvius31/OneShot OneShot
+
+execute_command chmod +x OneShot/oneshot.py
+
+printf "###############################################\n# All done! Now you can run OneShot with\n# sudo python OneShot/oneshot.py -i wlan0 -K\n#\n# To update, run\n# (cd OneShot && git pull)\n###############################################\n"
